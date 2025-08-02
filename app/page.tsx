a377ed7cbf4e13c70618974bc88202ea0a427144
Johxylon <captjohn.lohse@gmail.com>
2025-08-02 14:55:42 -0400

    Fix routing: redirect root to /waitlist and clean up duplicate files

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
-import ProvidersSection from '../components/ProvidersSection';
-import OrchestrationSection from '../components/OrchestrationSection';
-import IntegrationSection from '../components/IntegrationSection';
-import FeaturesSection from '../components/FeaturesSection';
-import PricingSection from '../components/PricingSection';
-import FAQSection from '../components/FAQSection';
-import CTASection from '../components/CTASection';
-import Footer from '../components/Footer';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <main className="min-h-[95vh]">
-      <Navbar />
-      <HeroSection />
-      <ProvidersSection />
-      <OrchestrationSection />
-      <IntegrationSection />
-      <FeaturesSection />
-      <PricingSection />
-      <FAQSection />
-      <CTASection />
-      <Footer />
-    </main>
-  );
+  redirect('/waitlist');
 } 
